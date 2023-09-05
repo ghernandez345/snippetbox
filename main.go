44d1b85f6ffdbf87c4b335a5595cdef2e448e83e
Gabriel Hernandez <ghernandez345@gmail.com>
2023-09-05 20:52:23 +0100

    add additional routes

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which wites a byte slice containing
-// "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -17,6 +28,8 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// User the http.ListenAndServe() function to start a new web server. We pass
 	// in two parameters: the TCP network address to listen on (in this case ":4000")
