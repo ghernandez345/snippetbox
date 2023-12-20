fb0e9066df27176a03ae129e8a9f228da28b7c40
Gabriel Hernandez <ghernandez345@gmail.com>
2023-12-19 22:15:37 -0600

    implement mysql connection:

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,12 +18,27 @@ type application struct {
 
 func main() {
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	password := os.Getenv("MYSQL_PASSWORD")
+	if password == "" {
+		errorLog.Fatal("MYSQL_PASSWORD environment variable not set")
+	}
+
+	dnsStr := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", password)
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", dnsStr, "MySQL data source name")
 
 	flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// application config
 	app := &application{
@@ -35,6 +54,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
