7a88613f81cd3cd851e115e28645a8aeccfe699d
Gabriel Hernandez <ghernandez345@gmail.com>
2023-12-21 21:33:11 -0600

    adding snippet modal

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,12 +8,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ghernandez345/snippetbox/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -44,6 +46,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// http server config
