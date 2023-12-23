3f984dcba0f2ae7da8092be7b8e234bffdbf01f2
Gabriel Hernandez <ghernandez345@gmail.com>
2023-12-23 14:49:42 -0600

    rewrite snippet get method

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -39,11 +39,9 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	row := m.DB.QueryRow(stmt, id)
-
 	s := &Snippet{}
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrNoRecord
