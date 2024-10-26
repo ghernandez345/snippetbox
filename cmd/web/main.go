801435b90b9a32abeb29fd366fb96bded29407c8
Gabriel Hernandez <ghernandez345@gmail.com>
2024-10-26 11:53:11 +0100

    stub out snippet model and models package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ghernandez345/snippetbox/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -30,7 +33,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", "addr", *addr)
