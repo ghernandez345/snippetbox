41ba54a5ddf502282778838d21c66f0827ee43ef
Gabriel Hernandez <ghernandez345@gmail.com>
2024-11-14 21:56:00 +0000

    add tls to server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -50,6 +50,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
@@ -67,7 +68,7 @@ func main() {
 
 	logger.Info("starting server", "addr", srv.Addr)
 
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
