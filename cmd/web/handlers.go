153e2b63cbcf35bb11cf309adfc7d9ac2b75c552
Gabriel Hernandez <ghernandez345@gmail.com>
2024-11-26 20:25:05 -0600

    add logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -205,5 +205,15 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serveError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
