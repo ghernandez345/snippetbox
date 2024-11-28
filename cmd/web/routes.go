217acdf44742421a97f4e2f19b25f32afd63f4f3
Gabriel Hernandez <ghernandez345@gmail.com>
2024-11-28 10:22:25 -0600

    embed static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/ghernandez345/snippetbox/ui"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
