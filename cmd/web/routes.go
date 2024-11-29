be05cfb18ed7a22e0db8609cfaa21b852e846758
Gabriel Hernandez <ghernandez345@gmail.com>
2024-11-29 11:50:04 -0600

    e2e handler tests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,6 +12,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
+	mux.HandleFunc("GET /ping", ping)
+
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
